1fad816ef40f5c22eab9cb604408ac444e1dc8d8
BrockBloomquist <104608812+BrockBloomquist@users.noreply.github.com>
2024-02-01 09:53:42 -0600

    Edited routes and added the categories and products side for the user/client end

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,6 @@ Rails.application.routes.draw do
   end
 
   get "admin" => "admin#index"
-  get "category" => "category#index"
+  resources :categories, only: [:show]
+  resources :products, only: [:show]
 end
